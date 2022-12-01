03eb4a21deaae71216ed9876b9d7272029364863
artisan <73726591+JaeHongDev@users.noreply.github.com>
2022-12-01 21:30:09 +0900

    Create Fail.java

---
+++ src/main/java/pairmatching/Fail.java
@@ -0,0 +1,37 @@
+package pairmatching;
+
+public class Fail {
+    private String a;
+    private String b;
+    private String c;
+    private String d;
+    private String e;
+
+    public void testA() {
+
+    }
+
+    public void testB() {
+
+    }
+
+    public void testC() {
+
+    }
+
+    public void testD() {
+
+    }
+
+    public void testE() {
+
+    }
+
+    public void testF() {
+
+    }
+
+    public void param(String a, String b, String c, String d) {
+        
+    }
+}
