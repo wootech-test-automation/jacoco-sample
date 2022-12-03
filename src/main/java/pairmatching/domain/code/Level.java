c5f997b94a64d3ceab400f69faeb704d6f3fa3a1
JaehongDev <println@kakao.com>
2022-12-03 14:05:18 +0900

    feature (Level) : 등록되지 않는 레벨 값이 들어오면 오류를 발생시키는기능 추가

--- src/main/java/pairmatching/domain/code/Level.java
+++ src/main/java/pairmatching/domain/code/Level.java
@@ -1,5 +1,7 @@
 package pairmatching.domain.code;
 
+import java.util.Arrays;
+
 public enum Level {
     LEVEL1("레벨1"),
     LEVEL2("레벨2"),
@@ -7,10 +9,18 @@ public enum Level {
     LEVEL4("레벨4"),
     LEVEL5("레벨5");
 
+    public static final String DID_NOT_EIXSTS_LEVEL_NAME = "존재하지 않는 입력입니다 :";
+    private static final String ERROR_MESSAGE = DID_NOT_EIXSTS_LEVEL_NAME + "[%s]";
     private final String name;
 
     Level(String name) {
         this.name = name;
+    }
+
+    public static Level of(final String input) {
+        return Arrays.stream(values()).filter(level -> level.name.equals(input))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(String.format(ERROR_MESSAGE, input)));
     }
 
     // 추가 기능 구현
