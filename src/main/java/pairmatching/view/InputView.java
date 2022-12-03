5f9a92213be22ee3b0b542d80cceb63a7e48001c
kat8813 <ohseo8813@gmail.com>
2022-12-03 13:57:48 +0900

    <feat>(InputView.java): 과정, 레벨, 미션 선택 기능 추가

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -4,12 +4,26 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private String selectProgramFunction() {
+    public String selectProgramFunction() {
         System.out.println("기능을 선택하세요.");
         System.out.println("1. 페어 매칭");
         System.out.println("2. 페어 조회");
         System.out.println("3. 페어 초기화");
         System.out.println("Q. 종료");
+        return Console.readLine();
+    }
+
+    public String selectSpecificPairOption() {
+        System.out.println("#############################################");
+        System.out.println("과정: 백엔드 | 프론트엔드");
+        System.out.println("미션:");
+        System.out.println("  - 레벨1: 자동차경주 | 로또 | 숫자야구게임");
+        System.out.println("  - 레벨2: 장바구니 | 결제 | 지하철노선도");
+        System.out.println("  - 레벨3: ");
+        System.out.println("  - 레벨4: 성능개선 | 배포");
+        System.out.println("  - 레벨5: ");
+        System.out.println("#############################################");
+        System.out.println("과정, 레벨, 미션을 선택하세요.");
         return Console.readLine();
     }
 }
