1f2ad76b673383a4297bf3da4275d9f3923992c1
JaehongDev <println@kakao.com>
2022-12-03 17:25:18 +0900

    feature (InputView) : 에러메시지 출력 기능 구현

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,9 +1,11 @@
 package pairmatching.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 import pairmatching.domain.code.Course;
 import pairmatching.domain.code.FunctionCommand;
 import pairmatching.domain.code.Mission;
+import pairmatching.domain.code.RematchingCommand;
 import pairmatching.domain.info.PairMatchingSelector;
 
 public class InputView extends IoPrinter {
@@ -23,11 +25,24 @@ public class InputView extends IoPrinter {
         this.println(String.format(MISSION_FORMAT, Mission.names()));
         this.println(DIVISION_LINE);
         this.println("과정, 레벨, 미션을 선택하세요");
-        return new PairMatchingSelector(Console.readLine());
+        return new PairMatchingSelector(readLine());
     }
 
     public FunctionCommand readPairMatchingServiceCommand() {
         this.println(CHOSE_FUNCTION);
-        return FunctionCommand.of(Console.readLine());
+        return FunctionCommand.of(readLine());
+    }
+
+    public RematchingCommand readReEnter() {
+        this.println("매칭 정보가 있습니다. 다시 매칭하시겠습니까?");
+        return RematchingCommand.of(readLine());
+    }
+
+    private String readLine() {
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException exception) {
+            throw new IllegalStateException("더 이상 입력이 불가능합니다.");
+        }
     }
 }
