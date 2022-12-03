7e43ed99b51394a1c12401bca03d5004a3ccf138
kat8813 <ohseo8813@gmail.com>
2022-12-03 14:11:33 +0900

    <refactor>(CrewNameReader.java): String이 아닌 Course를 통해 로직 처리하도록 변경

--- src/main/java/pairmatching/CrewNameReader.java
+++ src/main/java/pairmatching/CrewNameReader.java
@@ -5,26 +5,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import pairmatching.domain.Course;
 
 public class CrewNameReader {
 
-    private static final String BACKEND = "백엔드";
-    private static final String FRONTEND = "프론트엔드";
-    private static final String FILE_PATH_PREFIX = "src/main/resources";
-    private static final String BACKEND_PATH = "/backend-crew.md";
-    private static final String FRONTEND_PATH = "/frontend-crew.md";
-    private File crewNameFile;
-
-    public List<String> readCrewNames(String course) throws IOException {
-        if (course.equals(BACKEND)) {
-            crewNameFile = new File(FILE_PATH_PREFIX + BACKEND_PATH);
-        }
-
-        if (course.equals(FRONTEND)) {
-            crewNameFile = new File(FILE_PATH_PREFIX + FRONTEND_PATH);
-        }
-
+    public List<String> readCrewNames(Course course) throws IOException {
+        File crewNameFile = new File(course.getPath());
         Scanner scanner = new Scanner(crewNameFile);
+
         List<String> crewNames = new ArrayList<>();
         while (scanner.hasNextLine()) {
             crewNames.add(scanner.nextLine());
--- src/test/java/pairmatching/CrewNameReaderTest.java
+++ src/test/java/pairmatching/CrewNameReaderTest.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pairmatching.domain.Course;
 
 class CrewNameReaderTest {
 
@@ -14,14 +15,14 @@ class CrewNameReaderTest {
     @DisplayName("백엔드 크루 20명의 이름을 가져온다.")
     @Test
     void test1() throws IOException {
-        List<String> backendCrewNames = nameReader.readCrewNames("백엔드");
+        List<String> backendCrewNames = nameReader.readCrewNames(Course.BACKEND);
         assertThat(backendCrewNames.size()).isEqualTo(20);
     }
 
     @DisplayName("백엔드 크루들의 이름이 포함됐는지 확인한다. ")
     @Test
     void test2() throws IOException {
-        List<String> backendCrewNames = nameReader.readCrewNames("백엔드");
+        List<String> backendCrewNames = nameReader.readCrewNames(Course.BACKEND);
         assertThat(backendCrewNames.contains("백호")).isTrue();
         assertThat(backendCrewNames.contains("태웅")).isTrue();
         assertThat(backendCrewNames.contains("치수")).isTrue();
@@ -32,7 +33,7 @@ class CrewNameReaderTest {
     @DisplayName("프론트엔드 크루 15명의 이름을 가져온다.")
     @Test
     void test3() throws IOException {
-        List<String> frontendCrewNames = nameReader.readCrewNames("프론트엔드");
+        List<String> frontendCrewNames = nameReader.readCrewNames(Course.FRONTEND);
         assertThat(frontendCrewNames.size()).isEqualTo(15);
 
     }
@@ -40,7 +41,7 @@ class CrewNameReaderTest {
     @DisplayName("프론트엔드 크루들의 이름이 포함됐는지 확인한다. ")
     @Test
     void test4() throws IOException {
-        List<String> frontendCrewNames = nameReader.readCrewNames("프론트엔드");
+        List<String> frontendCrewNames = nameReader.readCrewNames(Course.FRONTEND);
         assertThat(frontendCrewNames.contains("보노")).isTrue();
         assertThat(frontendCrewNames.contains("시저")).isTrue();
         assertThat(frontendCrewNames.contains("쉐리")).isTrue();
