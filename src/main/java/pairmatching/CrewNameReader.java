e01e3bf9954f52241aa0663c433d9216fad82827
kat8813 <ohseo8813@gmail.com>
2022-12-03 13:48:43 +0900

    <feat>(CrewNameReader.java): 프론트엔드 크루의 이름을 읽을 수 있도록 추가

--- src/main/java/pairmatching/CrewNameReader.java
+++ src/main/java/pairmatching/CrewNameReader.java
@@ -1,7 +1,6 @@
 package pairmatching;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,11 +8,23 @@ import java.util.Scanner;
 
 public class CrewNameReader {
 
-    public List<String> readBackendCrewNames() throws IOException {
-        File file = new File(
-                "C:\\Users\\A\\Desktop\\Spring\\java-pairmatching-precourse-practice\\src\\main\\resources\\backend-crew.md");
-        FileReader fileReader = new FileReader(file);
-        Scanner scanner = new Scanner(file);
+    private static final String BACKEND = "백엔드";
+    private static final String FRONTEND = "프론트엔드";
+    private static final String FILE_PATH_PREFIX = "src/main/resources";
+    private static final String BACKEND_PATH = "/backend-crew.md";
+    private static final String FRONTEND_PATH = "/frontend-crew.md";
+    private File crewNameFile;
+
+    public List<String> readCrewNames(String course) throws IOException {
+        if (course.equals(BACKEND)) {
+            crewNameFile = new File(FILE_PATH_PREFIX + BACKEND_PATH);
+        }
+
+        if (course.equals(FRONTEND)) {
+            crewNameFile = new File(FILE_PATH_PREFIX + FRONTEND_PATH);
+        }
+
+        Scanner scanner = new Scanner(crewNameFile);
         List<String> crewNames = new ArrayList<>();
         while (scanner.hasNextLine()) {
             crewNames.add(scanner.nextLine());
