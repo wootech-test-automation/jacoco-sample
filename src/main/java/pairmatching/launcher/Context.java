f165b8350596b565e289e31226e7cfc3013611e3
JaehongDev <println@kakao.com>
2022-12-03 17:22:29 +0900

    feature (Context) : 컨텍스트의 반복횟수 기능 추가

--- src/main/java/pairmatching/launcher/Context.java
+++ src/main/java/pairmatching/launcher/Context.java
@@ -7,9 +7,13 @@ import pairmatching.domain.crew.MatchingResult;
 import pairmatching.domain.info.PairMatchingSelector;
 
 public class Context {
+    int count = 1;
     private MatchingResult matchingResult = new MatchingResult();
 
     public String matchPair(PairMatchingSelector pairMatchingSelector) {
+        if (matchingResult.findBeforeMatchingResult(pairMatchingSelector)) {
+            throw new IllegalStateException("매칭 정보가 이미 존재합니다");
+        }
         return matchedCrewToResult(matchingResult.addMatching(pairMatchingSelector));
     }
 
@@ -25,5 +29,13 @@ public class Context {
 
     public void resetMatchedPair() {
         this.matchingResult = new MatchingResult();
+    }
+
+    public int plusMatchCount() {
+        return count++;
+    }
+
+    public void resetMatchCount() {
+        this.count = 1;
     }
 }
