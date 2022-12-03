1a32160702316fc32f4ae27b1bc0dd0630208a52
kat8813 <ohseo8813@gmail.com>
2022-12-03 13:53:42 +0900

    <test>(CrewNameReaderTest.java): 읽어온 프론트엔드 크루들의 이름, 리스트 사이즈 테스트

--- src/test/java/pairmatching/CrewNameReaderTest.java
+++ src/test/java/pairmatching/CrewNameReaderTest.java
@@ -11,10 +11,38 @@ class CrewNameReaderTest {
 
     private CrewNameReader nameReader = new CrewNameReader();
 
-    @DisplayName("백엔드 크루 20명의 이름을 가져온다")
+    @DisplayName("백엔드 크루 20명의 이름을 가져온다.")
     @Test
     void test1() throws IOException {
-        List<String> backendCrewNames = nameReader.readBackendCrewNames();
+        List<String> backendCrewNames = nameReader.readCrewNames("백엔드");
         assertThat(backendCrewNames.size()).isEqualTo(20);
+    }
+
+    @DisplayName("백엔드 크루들의 이름이 포함됐는지 확인한다. ")
+    @Test
+    void test2() throws IOException {
+        List<String> backendCrewNames = nameReader.readCrewNames("백엔드");
+        assertThat(backendCrewNames.contains("백호")).isTrue();
+        assertThat(backendCrewNames.contains("태웅")).isTrue();
+        assertThat(backendCrewNames.contains("치수")).isTrue();
+        assertThat(backendCrewNames.contains("태섭")).isTrue();
+        assertThat(backendCrewNames.contains("대만")).isTrue();
+    }
+
+    @DisplayName("프론트엔드 크루 15명의 이름을 가져온다.")
+    @Test
+    void test3() throws IOException {
+        List<String> frontendCrewNames = nameReader.readCrewNames("프론트엔드");
+        assertThat(frontendCrewNames.size()).isEqualTo(15);
+
+    }
+
+    @DisplayName("프론트엔드 크루들의 이름이 포함됐는지 확인한다. ")
+    @Test
+    void test4() throws IOException {
+        List<String> frontendCrewNames = nameReader.readCrewNames("프론트엔드");
+        assertThat(frontendCrewNames.contains("보노")).isTrue();
+        assertThat(frontendCrewNames.contains("시저")).isTrue();
+        assertThat(frontendCrewNames.contains("쉐리")).isTrue();
     }
 }
