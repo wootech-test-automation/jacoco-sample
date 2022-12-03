4457a9e5e581d09554ba1d0268b08285c4ef8a4d
JaehongDev <println@kakao.com>
2022-12-03 17:22:51 +0900

    feature (Application) : 어플리케이션에서 런처를 실행시키는 코드 작성

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,8 +1,12 @@
 package pairmatching;
 
+import pairmatching.launcher.PairmatchingLauncher;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+
+        new PairmatchingLauncher().execute();
 
     }
 }
