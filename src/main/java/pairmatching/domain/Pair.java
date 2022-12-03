3d431a2752caea93292aa47ba335f3fe01009e38
kat8813 <ohseo8813@gmail.com>
2022-12-03 16:46:15 +0900

    <feat>(Pair.java): 홀수일때 처리하는 기능 추가

--- src/main/java/pairmatching/domain/Pair.java
+++ src/main/java/pairmatching/domain/Pair.java
@@ -22,8 +22,20 @@ public class Pair {
                 .collect(Collectors.toList());
     }
 
-    //TODO 홀수 처리 기능
+    //TODO 리팩토링
     private static List<Pair> pairMatching(List<Crew> crews) {
+        if (crews.size() % 2 == 1) {
+            return IntStream.range(0, crews.size())
+                    .limit(crews.size()-1)
+                    .filter(i -> i % 2 == 0)
+                    .mapToObj(idx -> {
+                        if (idx == crews.size() - 3) {
+                            return new Pair((crews.get(idx)), crews.get(idx + 1), crews.get(idx + 2));
+                        }
+                        return new Pair(crews.get(idx), crews.get(idx + 1));
+                    })
+                    .collect(Collectors.toList());
+        }
         return IntStream.range(0, crews.size())
                 .filter(i -> i % 2 == 0)
                 .mapToObj(idx -> new Pair(crews.get(idx), crews.get(idx + 1)))
