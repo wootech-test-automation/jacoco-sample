8c1c39ce54555994932a3ceae6690ca65165f7e4
JaehongDev <println@kakao.com>
2022-12-03 14:20:28 +0900

    feature (PairMatchingSelector) : 페어매칭의 입력값에 대한 구분값을 콤마로 구분하는 기능 추가

---
+++ src/main/java/pairmatching/domain/info/PairMatchingSelector.java
@@ -0,0 +1,28 @@
+package pairmatching.domain.info;
+
+import pairmatching.domain.code.Course;
+import pairmatching.domain.code.Level;
+import pairmatching.domain.code.Subject;
+
+public class PairMatchingSelector {
+    public static final String DID_NOT_SPLIT_INPUT = "콤마로 구분할경우 3가지가 입려된 것이 아닙니다.";
+    private static final String ERROR_MESSAGE = DID_NOT_SPLIT_INPUT + "[input %s]";
+    private final Level level;
+    private final Course course;
+    private final Subject subject;
+
+    public PairMatchingSelector(final String input) {
+        var inputStr = input.split(",");
+        validateInputSize(inputStr.length);
+
+        this.level = Level.of(inputStr[0]);
+        this.course = Course.of(inputStr[1]);
+        this.subject = Subject.of(inputStr[2]);
+    }
+
+    private void validateInputSize(int size) {
+        if (size != 3) {
+            throw new IllegalArgumentException(String.format(ERROR_MESSAGE, size));
+        }
+    }
+}
