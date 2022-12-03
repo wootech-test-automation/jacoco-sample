c849bcbf924e1326d8bc5211cbfd3f96c7badc89
JaehongDev <println@kakao.com>
2022-12-03 17:25:32 +0900

    feature (OutputView) : 초기화 메시지 구현

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -4,4 +4,12 @@ public class OutputView extends IoPrinter {
     public void printMatchingResult(String matchPair) {
         this.println(matchPair);
     }
+
+    public void printReset() {
+        this.println("초기화 되었습니다.");
+    }
+
+    public void printError(String message) {
+        this.println("[ERROR]" + message);
+    }
 }
