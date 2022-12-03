e37fab369c0f2c0c833e51cf2573669c40b7cf5c
kat8813 <ohseo8813@gmail.com>
2022-12-03 17:34:01 +0900

    <refactor>(Pair.java): 페어 매칭 코드 메소드 추출로 간략화

--- src/main/java/pairmatching/domain/Pair.java
+++ src/main/java/pairmatching/domain/Pair.java
@@ -22,20 +22,27 @@ public class Pair {
                 .collect(Collectors.toList());
     }
 
-    //TODO 리팩토링
     private static List<Pair> pairMatching(List<Crew> crews) {
-        if (crews.size() % 2 == 1) {
-            return IntStream.range(0, crews.size())
-                    .limit(crews.size()-1)
-                    .filter(i -> i % 2 == 0)
-                    .mapToObj(idx -> {
-                        if (idx == crews.size() - 3) {
-                            return new Pair((crews.get(idx)), crews.get(idx + 1), crews.get(idx + 2));
-                        }
-                        return new Pair(crews.get(idx), crews.get(idx + 1));
-                    })
-                    .collect(Collectors.toList());
+        if (crews.size() % 2 == 0) {
+            return getPairsIfEven(crews);
         }
+        return getPairsIfOdds(crews);
+    }
+
+    private static List<Pair> getPairsIfOdds(List<Crew> crews) {
+        return IntStream.range(0, crews.size())
+                .limit(crews.size() - 1)
+                .filter(i -> i % 2 == 0)
+                .mapToObj(idx -> {
+                    if (idx == crews.size() - 3) {
+                        return new Pair((crews.get(idx)), crews.get(idx + 1), crews.get(idx + 2));
+                    }
+                    return new Pair(crews.get(idx), crews.get(idx + 1));
+                })
+                .collect(Collectors.toList());
+    }
+
+    private static List<Pair> getPairsIfEven(List<Crew> crews) {
         return IntStream.range(0, crews.size())
                 .filter(i -> i % 2 == 0)
                 .mapToObj(idx -> new Pair(crews.get(idx), crews.get(idx + 1)))
