b6d012f342545d8d25c93d343949251e29c25819
JaehongDev <println@kakao.com>
2022-12-04 16:52:06 +0900

    feature (PairMatchingContext) : 반복횟수를 추가하는 기능 구현

--- src/main/java/pairmatching/system/PairmatchingContext.java
+++ src/main/java/pairmatching/system/PairmatchingContext.java
@@ -1,5 +1,12 @@
 package pairmatching.system;
 
+import pairmatching.matching.MatchingDivision;
+
 public interface PairmatchingContext {
 
+    void requestMatching(MatchingDivision matchingDivision);
+
+    String matchPair();
+
+    void retryMatching();
 }
--- src/main/java/pairmatching/system/PairmatchingContextImpl.java
+++ src/main/java/pairmatching/system/PairmatchingContextImpl.java
@@ -1,4 +1,29 @@
 package pairmatching.system;
 
+import pairmatching.domain.count.RepeatMatchingCount;
+import pairmatching.matching.MatchingDivision;
+import pairmatching.matching.MatchingResult;
+
 public class PairmatchingContextImpl implements PairmatchingContext {
+
+    private final MatchingResult matchingResult = new MatchingResult();
+    private MatchingDivision matchingDivision;
+    private RepeatMatchingCount repeatMatchingCount = RepeatMatchingCount.initializeRepeatMatchingCount();
+
+    @Override
+    public void requestMatching(MatchingDivision matchingDivision) {
+        this.matchingDivision = matchingDivision;
+
+    }
+
+    @Override
+    public String matchPair() {
+        this.matchingResult.matchPair(matchingDivision, matchingDivision.getShuffledCrew());
+        return this.matchingResult.findMatchingPair(matchingDivision);
+    }
+
+    @Override
+    public void retryMatching() {
+        repeatMatchingCount = repeatMatchingCount.plusCount();
+    }
 }
