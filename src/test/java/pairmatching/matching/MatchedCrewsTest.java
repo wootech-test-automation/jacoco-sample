5803f825658c84eda5ee0c9b6c6f7f2d7e261709
JaehongDev <println@kakao.com>
2022-12-04 16:52:32 +0900

    test ( MatchedCrews) : 크루를 매칭하는 기능 구현

---
+++ src/test/java/pairmatching/matching/MatchedCrewsTest.java
@@ -0,0 +1,35 @@
+package pairmatching.matching;
+
+import static pairmatching.utils.TestUtils.stringToCrews;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class MatchedCrewsTest {
+
+    @Test
+    void 크루의_수가_홀수일경우_마지막_페어는_3명입니다() {
+        var matchedCrew = new MatchedCrews(stringToCrews("가,나,다,라,마,바,사"));
+        var actual = matchedCrew.result().split("\n");
+
+        Assertions.assertThat(actual[0]).isEqualTo("가 : 나");
+        Assertions.assertThat(actual[1]).isEqualTo("다 : 라");
+        Assertions.assertThat(actual[2]).isEqualTo("마 : 바 : 사");
+
+    }
+
+    @Test
+    void 크루의_수가_짝수일경우_입니다() {
+        var matchedCrew = new MatchedCrews(stringToCrews("가,나,다,라,마,바"));
+        var actual = matchedCrew.result().split("\n");
+
+        Assertions.assertThat(actual[0]).isEqualTo("가 : 나");
+        Assertions.assertThat(actual[1]).isEqualTo("다 : 라");
+        Assertions.assertThat(actual[2]).isEqualTo("마 : 바");
+    }
+
+
+}
