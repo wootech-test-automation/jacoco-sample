fd324b5972fe3f5e8737e752bb473a5f5a646a53
JaehongDev <println@kakao.com>
2022-12-04 13:27:18 +0900

    refactor (FeatursCommandCode) : 매칭 기능에 대한 코드 인터페이스로 중복된 기능 제거

--- src/main/java/pairmatching/code/FeaturesCommandCode.java
+++ src/main/java/pairmatching/code/FeaturesCommandCode.java
@@ -3,14 +3,12 @@ package pairmatching.code;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public enum FeaturesCommandCode {
+public enum FeaturesCommandCode implements InputCommand {
     MATCHING("1", "페어 매칭"),
     SELECT("2", "페어 조회"),
     INITIALIZE("3", "페어 초기화"),
     QUIT("Q", "종료");
 
-    public static final String DID_NOT_EXISTS_CODE_NAME = "해당 명령어는 존재하지 않습니다";
-    private static final String ERROR_MESSAGE = DID_NOT_EXISTS_CODE_NAME + "[input : %s]";
     private static final String MESSAGE_FORMAT = "%s. %s";
     private final String command;
     private final String message;
@@ -20,7 +18,7 @@ public enum FeaturesCommandCode {
         this.message = message;
     }
 
-    public static FeaturesCommandCode of(String input) {
+    public static FeaturesCommandCode of(final String input) {
         return Arrays.stream(values())
                 .filter(featuresCommandCode -> featuresCommandCode.command.equals(input.trim()))
                 .findAny()
@@ -35,5 +33,9 @@ public enum FeaturesCommandCode {
 
     private String getMessage() {
         return String.format(MESSAGE_FORMAT, command, message);
+    }
+
+    public boolean isMatching() {
+        return this == MATCHING;
     }
 }
