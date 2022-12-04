c8491cc77811c814028ba0b6f55d27590ca75cdc
JaehongDev <println@kakao.com>
2022-12-04 16:45:02 +0900

    feature (MatchingResult) : 같은 레벨에서 매칭된적이 있는 크루를 검증하는 기능 추가

--- src/main/java/pairmatching/matching/MatchingResult.java
+++ src/main/java/pairmatching/matching/MatchingResult.java
@@ -1,14 +1,36 @@
 package pairmatching.matching;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MatchingResult {
     private final Map<MatchingDivision, MatchedCrews> matchingResult = new HashMap<>();
 
-
-    public void matchPair(MatchingDivision matchingDivision) {
-        this.matchingResult.put(matchingDivision, new MatchedCrews(matchingDivision.getShuffledCrew()));
+    public void matchPair(MatchingDivision matchingDivision, List<Crew> crews) {
+        validateBeforeMatchingCrew(matchingDivision);
+        var matchedCrews = new MatchedCrews(crews);
+        this.validateSameMatchingCrew(matchingDivision, matchedCrews);
+        this.matchingResult.put(matchingDivision, matchedCrews);
     }
 
+    private void validateBeforeMatchingCrew(MatchingDivision matchingDivision) {
+        if (this.matchingResult.containsKey(matchingDivision)) {
+            throw new IllegalStateException("이미 기록이 존재합니다.");
+        }
+    }
+    
+
+    private void validateSameMatchingCrew(MatchingDivision matchingDivision, MatchedCrews matchedCrews) {
+        var result = matchingResult.entrySet().stream()
+                .filter(matchedCrewsEntry -> matchedCrewsEntry.getKey().isSameLevel(matchingDivision))
+                .anyMatch(matchedCrewsEntry -> matchedCrewsEntry.getValue().isBeforeMatchingCrews(matchedCrews));
+        if (result) {
+            throw new IllegalStateException("이전에 매칭된 경험이 있는 크루입니다.");
+        }
+    }
+
+    public String findMatchingPair(MatchingDivision matchingDivision) {
+        return this.matchingResult.get(matchingDivision).result();
+    }
 }
