b8c92443e323d43c4d7c8001ba24ca627983bc77
JaehongDev <println@kakao.com>
2022-12-04 17:34:25 +0900

    feature (RetryMatchingStatus) : 매칭을 반복하는 상태 구현

--- src/main/java/pairmatching/system/status/RetryProcessMatchingPairStatus.java
+++ src/main/java/pairmatching/system/status/RetryProcessMatchingPairStatus.java
@@ -1,2 +1,23 @@
-package pairmatching.system.status;public class RetryProcessMatchingPairStatus {
+package pairmatching.system.status;
+
+import pairmatching.system.PairmatchingContext;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
+public class RetryProcessMatchingPairStatus implements PairmatchingStatus {
+    @Override
+    public PairmatchingStatus next(PairmatchingContext pairmatchingContext, InputView inputView,
+                                   OutputView outputView) {
+        var command = inputView.readRetryCommand();
+        System.out.println(command.isRetry());
+        if (command.isRetry()) {
+            return new MatchingPairStatus();
+        }
+        return new ExitStatus();
+    }
+
+    @Override
+    public boolean runnable() {
+        return true;
+    }
 }
