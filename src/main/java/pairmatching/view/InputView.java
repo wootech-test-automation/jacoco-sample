421d05ad07a28fb5b1d75fb2786673e85a312ffc
JaehongDev <println@kakao.com>
2022-12-04 13:10:15 +0900

    feature (InputView) : 기능선택 문자열을 입력받는 기능 추가

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,4 +1,22 @@
 package pairmatching.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
+import pairmatching.code.FeaturesCommandCode;
+
 public class InputView extends IoPrinter {
+
+    public FeaturesCommandCode readFeatureCommand() {
+        this.println("기능을 선택하세요.");
+        this.println(FeaturesCommandCode.getMessages());
+        return FeaturesCommandCode.of(readLine());
+    }
+
+    private String readLine() {
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException exception) {
+            throw new IllegalStateException();
+        }
+    }
 }
