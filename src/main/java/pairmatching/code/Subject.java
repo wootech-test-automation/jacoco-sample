39668b8851446706035043eeaf0d8974fac73c44
JaehongDev <println@kakao.com>
2022-12-04 13:33:17 +0900

    feature (Subject) : 교과목에 대한 기능 구현

---
+++ src/main/java/pairmatching/code/Subject.java
@@ -0,0 +1,28 @@
+package pairmatching.code;
+
+import java.util.Arrays;
+
+public enum Subject implements InputCommand {
+    RACING("자동차경주"),
+    LOTTO("로또"),
+    BASEBALL("숫자야구게임"),
+    SHOPPING("장바구니"),
+    PAYMENTS("결제"),
+    SUBWAY("지하철노선도"),
+    IMPROVEMENT_PERFORMANCE("성능개선"),
+    DEPLOY("배포");
+
+    private final String name;
+
+    Subject(final String name) {
+        this.name = name;
+    }
+
+    public static Subject of(final String input) {
+        return Arrays.stream(values())
+                .filter(subject -> subject.name.equals(input.trim()))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(String.format(ERROR_MESSAGE + input)));
+    }
+
+}
