7f55b0b1794271a4d46ec31ed42a40ab89ca6732
JaehongDev <println@kakao.com>
2022-12-04 15:47:00 +0900

    feature (Crew) : 크루 클래스 추가

---
+++ src/main/java/pairmatching/matching/Crew.java
@@ -0,0 +1,35 @@
+package pairmatching.matching;
+
+import java.util.Objects;
+import pairmatching.code.Course;
+
+public class Crew {
+    private final Course course;
+    private final String name;
+
+    public Crew(final Course course, final String name) {
+        this.course = course;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Crew crew = (Crew) o;
+        return course == crew.course && Objects.equals(name, crew.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(course, name);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
