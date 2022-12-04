444d258aac044b43cd7e15d2f6a1aecefe81ae30
JaehongDev <println@kakao.com>
2022-12-04 13:10:40 +0900

    refactor (PairmatchingLauncher) : 페어매칭 상태별로 이동하는 기능으로 변경

--- src/main/java/pairmatching/system/PairmatchingLauncher.java
+++ src/main/java/pairmatching/system/PairmatchingLauncher.java
@@ -7,22 +7,22 @@ import pairmatching.view.OutputView;
 
 public class PairmatchingLauncher {
 
-    private final PairmatchingContext pairtmatchingContext;
+    private final PairmatchingContext pairmatchingContext;
     private final InputView inputView;
     private final OutputView outputView;
 
-    private final PairmatchingStatus pairmatchingStatus = new InitStatus();
+    private PairmatchingStatus pairmatchingStatus = new InitStatus();
 
     public PairmatchingLauncher(final PairmatchingContext pairmatchingContext, final InputView inputView,
                                 final OutputView outputView) {
-        this.pairtmatchingContext = pairmatchingContext;
+        this.pairmatchingContext = pairmatchingContext;
         this.inputView = inputView;
         this.outputView = outputView;
     }
 
     public void execute() {
         while (pairmatchingStatus.runnable()) {
-            pairmatchingStatus.next(pairtmatchingContext, inputView, outputView);
+            pairmatchingStatus = pairmatchingStatus.next(pairmatchingContext, inputView, outputView);
         }
 
     }
