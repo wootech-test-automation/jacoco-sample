10f4c302bfa9241e0156cfe3e8eb9dd8e265e7e9
JaehongDev <println@kakao.com>
2022-12-04 16:33:44 +0900

    feature (MatchedCrews) : 매칭된 이력이 있는 크루를 검증하는 코드 작성

--- src/main/java/pairmatching/matching/MatchedCrew.java
+++ src/main/java/pairmatching/matching/MatchedCrew.java
@@ -1,5 +1,6 @@
 package pairmatching.matching;
 
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -18,4 +19,20 @@ public class MatchedCrew {
                 .map(Crew::getName)
                 .collect(Collectors.joining(" : "));
     }
+
+    public boolean isBeforeMatchingCrews(MatchedCrew targetMatchedCrews) {
+        var union = new HashSet<>(matchedCrew);
+        union.retainAll(targetMatchedCrews.matchedCrew);
+        return union.size() >= 2;
+    }
+
+    // 3 2
+    // 3 2
+    // []
+    // 3 1 2
+    // 4 1 2
+    // 3
+    // 3 2 1
+    // 3 2
+    // 1
 }
