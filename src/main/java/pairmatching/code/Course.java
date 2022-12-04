3455e02ba7c2546ceee64ad79f7f32bd90725964
JaehongDev <println@kakao.com>
2022-12-04 15:46:31 +0900

    feature (Course) : 과정에서 파일을 들고 있도록 변경

--- src/main/java/pairmatching/code/Course.java
+++ src/main/java/pairmatching/code/Course.java
@@ -1,15 +1,22 @@
 package pairmatching.code;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import pairmatching.files.ReadFile;
 
 public enum Course implements InputCommand {
-    BACKEND("백엔드"),
-    FRONTEND("프론트엔드");
+    BACKEND("백엔드", "backend-crew.md"),
+    FRONTEND("프론트엔드", "frontend-crew.md");
 
+    private static final String DELIMITER = " | ";
     private final String name;
+    private final String path;
 
-    Course(final String name) {
+    Course(final String name, final String path) {
         this.name = name;
+        this.path = path;
     }
 
     public static Course of(final String input) {
@@ -17,5 +24,15 @@ public enum Course implements InputCommand {
                 .filter(course -> course.name.equals(input.trim()))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException(String.format(ERROR_MESSAGE, input)));
+    }
+
+    public static String messages() {
+        return Arrays.stream(values())
+                .map(course -> course.name)
+                .collect(Collectors.joining(DELIMITER));
+    }
+
+    public List<String> findCrewByCourses() {
+        return Randoms.shuffle(ReadFile.readFileByCourse(path));
     }
 }
