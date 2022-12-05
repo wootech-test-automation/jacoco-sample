8e429d9b933045927e6139c28c3b78cd01622905
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-05 20:59:06 +0900

    refactor(consoleReader): change object to domain class

--- src/main/java/pairmatching/util/io/ConsoleReader.java
+++ src/main/java/pairmatching/util/io/ConsoleReader.java
@@ -1,29 +1,20 @@
 package pairmatching.util.io;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.List;
-import pairmatching.domain.type.Course;
 import pairmatching.domain.type.Feature;
-import pairmatching.domain.type.Level;
-import pairmatching.domain.type.Mission;
+import pairmatching.domain.type.MatchingInformation;
 import pairmatching.domain.type.YesOrNo;
 
 public class ConsoleReader {
-
-    private static final String COMMA = ",";
 
     public static Feature readFeature() {
         String line = Console.readLine();
         return Feature.matchOf(line);
     }
 
-    public static List<Object> readInformation() {
+    public static MatchingInformation readInformation() {
         String line = Console.readLine();
-        String[] split = line.split(COMMA);
-        Course course = Course.matchOf(split[0].trim());
-        Level level = Level.matchOf(split[1].trim());
-        Mission mission = Mission.matchOf(split[2].trim(), level);
-        return List.of(course, level, mission);
+        return MatchingInformation.of(line);
     }
 
     public static YesOrNo readYseOrNo() {
