7ec10edc72d8955c26ee0f19cc6e2099f21bdec9
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-05 20:57:33 +0900

    fix(mission): change field type

--- src/main/java/pairmatching/domain/type/Mission.java
+++ src/main/java/pairmatching/domain/type/Mission.java
@@ -1,51 +1,36 @@
 package pairmatching.domain.type;
 
-import java.util.Arrays;
+
 import java.util.stream.Stream;
 
 public enum Mission {
-    RACING_CAR("자동차경주", Level.LEVEL1),
-    LOTTO("로또", Level.LEVEL1),
-    BASE_BALL("숫자야구게임", Level.LEVEL1),
-    BASKET("장바구니", Level.LEVEL2),
-    CREDIT("결제", Level.LEVEL2),
-    SUBWAY("지하철노선도", Level.LEVEL2),
-    IMPROVEMENT("성능개선", Level.LEVEL4),
-    DISTRIBUTE("배포", Level.LEVEL4);
+    RACING_CAR(Subject.RACING_CAR, Level.LEVEL1),
+    LOTTO(Subject.LOTTO, Level.LEVEL1),
+    BASE_BALL(Subject.BASE_BALL, Level.LEVEL1),
+    BASKET(Subject.BASKET, Level.LEVEL2),
+    CREDIT(Subject.CREDIT, Level.LEVEL2),
+    SUBWAY(Subject.SUBWAY, Level.LEVEL2),
+    IMPROVEMENT(Subject.IMPROVEMENT, Level.LEVEL4),
+    DISTRIBUTE(Subject.DISTRIBUTE, Level.LEVEL4);
 
     private static final String INVALID_MISSION_NAME = "레벨에 맞는 정확한 미션 이름을 입력해주세요.";
-    private final String name;
+    private final Subject subject;
     private final Level level;
 
-    Mission(String name, Level level) {
-        this.name = name;
+    Mission(Subject subject, Level level) {
+        this.subject = subject;
         this.level = level;
     }
 
-    public static Mission matchOf(String name) {
+    public static Mission matchOf(Level level, Subject subject) {
         return Stream.of(Mission.values())
-                .filter(mission -> name.equals(mission.name))
+                .filter(mission -> subject.equals(mission.subject) && level.equals(mission.level))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException(INVALID_MISSION_NAME));
-    }
-
-    public static Mission matchOf(String name, Level level) {
-        return Stream.of(Mission.values())
-                .filter(mission -> name.equals(mission.name) && level.equals(mission.level))
-                .findAny()
-                .orElseThrow(() -> new IllegalArgumentException(INVALID_MISSION_NAME));
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Level getLevel() {
-        return level;
     }
 
     @Override
     public String toString() {
-        return level + ":" + name;
+        return level + ":" + subject;
     }
 }
