f09cbe0aa09c27880cf6ceb47982ac257106ad92
JaehongDev <println@kakao.com>
2022-12-08 14:17:20 +0900

    feature (Subject) : 과목의 이름을 반환하는 기능 추가

--- src/main/java/pairmatching/domain/code/Subject.java
+++ src/main/java/pairmatching/domain/code/Subject.java
@@ -3,7 +3,14 @@ package pairmatching.domain.code;
 import java.util.Arrays;
 
 public enum Subject {
-    RACING("자동차경주"), LOTTO("로또"), BASEBALL("숫자야구게임"), SHOPPING("장바구니"), PAYMENT("결제"), SUBWAY("지하철노선도"), IMPROVE_PERFORMANCE("성능개선"), DEPLOY("배포");
+    RACING("자동차경주"),
+    LOTTO("로또"),
+    BASEBALL("숫자야구게임"),
+    SHOPPING("장바구니"),
+    PAYMENT("결제"),
+    SUBWAY("지하철노선도"),
+    IMPROVE_PERFORMANCE("성능개선"),
+    DEPLOY("배포");
 
 
     private final String name;
@@ -17,5 +24,9 @@ public enum Subject {
                 .filter(subject -> subject.name.equals(input.trim()))
                 .findAny()
                 .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public String getName() {
+        return name;
     }
 }
