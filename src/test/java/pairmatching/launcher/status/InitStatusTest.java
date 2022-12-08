87920feb4d7358d9df21e964f5328010f15fe372
JaehongDev <println@kakao.com>
2022-12-08 10:44:25 +0900

    test (InitStatus) : 초기상태에서 가짜 컨텍스트를 사용하도록 변경

--- src/test/java/pairmatching/launcher/status/InitStatusTest.java
+++ src/test/java/pairmatching/launcher/status/InitStatusTest.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
-import pairmatching.launcher.context.PairmatchingContextImpl;
+import pairmatching.launcher.context.FakePairmatchingContext;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class InitStatusTest {
 
     @Test
     void 초기화_상태_다음은_사용자_기능선택_상태입니다() {
-        var actualStatus = new InitStatus().next(new PairmatchingContextImpl());
+        var actualStatus = new InitStatus().next(new FakePairmatchingContext());
         var expectedStatus = new SelectFeatureStatus();
         Assertions.assertThat(actualStatus).isInstanceOf(expectedStatus.getClass());
 
