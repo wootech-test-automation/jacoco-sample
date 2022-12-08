3be1a09eaeb7f16588e212a50630a707f20d882e
JaehongDev <println@kakao.com>
2022-12-08 10:43:47 +0900

    feature (PairmatchingContext) : 컨텍스트에서 뷰를 전달받는 기능 구현

--- src/main/java/pairmatching/launcher/context/PairmatchingContext.java
+++ src/main/java/pairmatching/launcher/context/PairmatchingContext.java
@@ -1,4 +1,9 @@
 package pairmatching.launcher.context;
 
+import pairmatching.view.PairmatchingView;
+
 public interface PairmatchingContext {
+
+
+    PairmatchingView getPairmatchingView();
 }
--- src/main/java/pairmatching/launcher/context/PairmatchingContextImpl.java
+++ src/main/java/pairmatching/launcher/context/PairmatchingContextImpl.java
@@ -1,4 +1,15 @@
 package pairmatching.launcher.context;
 
+import pairmatching.view.PairmatchingView;
+
 public class PairmatchingContextImpl implements PairmatchingContext {
+    private final PairmatchingView pairmatchingView;
+
+    public PairmatchingContextImpl(PairmatchingView pairmatchingView) {
+        this.pairmatchingView = pairmatchingView;
+    }
+
+    public PairmatchingView getPairmatchingView() {
+        return this.pairmatchingView;
+    }
 }
