2a604d6b249ab04d6de4cf75959dfd748e4684a2
JaehongDev <println@kakao.com>
2022-12-08 15:55:05 +0900

    feature (Crew) : 크루에 대한 기능 구현

---
+++ src/main/java/pairmatching/domain/matching/Crew.java
@@ -0,0 +1,41 @@
+package pairmatching.domain.matching;
+
+import java.util.Objects;
+import pairmatching.domain.code.Course;
+
+public class Crew {
+    private static final int MIN_SIZE = 1;
+    private final Course course;
+    private final String name;
+
+    public Crew(Course course, String name) {
+        var tempName = name.trim();
+        validateNameSizeIsGreaterThanMinSize(tempName.length());
+        this.course = course;
+        this.name = name;
+    }
+
+    private void validateNameSizeIsGreaterThanMinSize(int length) {
+        if (length < MIN_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Crew crew = (Crew) o;
+        return course == crew.course && Objects.equals(name, crew.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(course, name);
+    }
+}
