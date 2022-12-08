fce867a909c4f3573f97f35c736e3b1116e9ecee
JaehongDev <println@kakao.com>
2022-12-08 10:54:50 +0900

    test (FeatureCommandTest) :  기능목록이 정상적으로 반환되는지 검증하는 테스트코드 작성

--- src/test/java/pairmatching/launcher/code/FeatureCommandTest.java
+++ src/test/java/pairmatching/launcher/code/FeatureCommandTest.java
@@ -3,6 +3,7 @@ package pairmatching.launcher.code;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,4 +16,13 @@ class FeatureCommandTest {
         Assertions.assertThatIllegalArgumentException().isThrownBy(() -> FeatureCommand.from(input));
     }
 
+    @Test
+    void 기능명령어는_메시지를_가집니다() {
+        var actual = FeatureCommand.messages();
+        var expected = "1. 페어 매칭\n" +
+                "2. 페어 조회\n" +
+                "3. 페어 초기화\n" +
+                "Q. 종료";
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
 }
