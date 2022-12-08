e7e0137d8f1abf503046d5958d8908a6916297f4
JaehongDev <println@kakao.com>
2022-12-08 13:53:32 +0900

    refactor (Subject) : 과제 추가

--- src/main/java/pairmatching/domain/code/Subject.java
+++ src/main/java/pairmatching/domain/code/Subject.java
@@ -3,7 +3,8 @@ package pairmatching.domain.code;
 import java.util.Arrays;
 
 public enum Subject {
-    RACING("자동차경주"), LOTTO("로또"), BASEBALL("숫자야구게임"), SHOPPING("장바구니"), PAYMENT("결제"), IMPROVE_PERFORMANCE("지하철노선도"), DEPLOY("성능개선");
+    RACING("자동차경주"), LOTTO("로또"), BASEBALL("숫자야구게임"), SHOPPING("장바구니"), PAYMENT("결제"), SUBWAY("지하철노선도"), IMPROVE_PERFORMANCE("성능개선"), DEPLOY("배포");
+
 
     private final String name;
 
