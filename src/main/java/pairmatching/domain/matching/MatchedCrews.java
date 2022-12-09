6a41f5f6d72d313a7d0758338f66f37878234ccc
JaehongDev <println@kakao.com>
2022-12-09 10:11:40 +0900

    feature (MatchedCrews) : 매칭된 크루 목록에 대한 결과를 반환하는 기능 추가

--- src/main/java/pairmatching/domain/matching/MatchedCrews.java
+++ src/main/java/pairmatching/domain/matching/MatchedCrews.java
@@ -2,6 +2,7 @@ package pairmatching.domain.matching;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MatchedCrews {
     private final List<MatchedCrew> matchedCrewList;
@@ -10,8 +11,13 @@ public class MatchedCrews {
         this.matchedCrewList = new ArrayList<>();
     }
 
-
     public void add(MatchedCrew matchedCrew) {
         this.matchedCrewList.add(matchedCrew);
+    }
+
+    public String result() {
+        return this.matchedCrewList.stream()
+                .map(MatchedCrew::result)
+                .collect(Collectors.joining("\n"));
     }
 }
