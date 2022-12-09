3429bc8ca9e766ca83eae652d2d09cef721d4f33
JaehongDev <println@kakao.com>
2022-12-09 15:29:26 +0900

    test

--- src/test/java/pairmatching/launcher/context/FakePairmatchingContext.java
+++ src/test/java/pairmatching/launcher/context/FakePairmatchingContext.java
@@ -1,17 +1,49 @@
 package pairmatching.launcher.context;
 
 import pairmatching.domain.matching.MatchingDivision;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
 import pairmatching.view.PairmatchingView;
 
 public class FakePairmatchingContext implements PairmatchingContext {
 
     @Override
     public PairmatchingView getPairmatchingView() {
-        return null;
+        return new PairmatchingView(new InputView(), new OutputView());
     }
 
     @Override
     public void inputMatchingDivision(MatchingDivision matchingDivision) {
+
+    }
+
+    @Override
+    public String matchPair() {
+        return null;
+    }
+
+    @Override
+    public String rematchPair() {
+        return null;
+    }
+
+    @Override
+    public String findMatchedCrewsByMatchingDivision() {
+        return null;
+    }
+
+    @Override
+    public void plusTryCount() {
+
+    }
+
+    @Override
+    public void initializeRetryCount() {
+
+    }
+
+    @Override
+    public void initializeMatchingResult() {
 
     }
 }
--- src/test/java/pairmatching/launcher/status/SelectFeatureStatusTest.java
+++ src/test/java/pairmatching/launcher/status/SelectFeatureStatusTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import pairmatching.launcher.code.MatchingInitializeStatus;
 import pairmatching.launcher.context.FakePairmatchingContext;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
