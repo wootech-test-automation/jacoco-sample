9fffc9a59745653e8e1de7c0cd87303670a3cb7a
JaehongDev <println@kakao.com>
2022-12-09 13:21:25 +0900

    feature (OutputView) : 매칭 결과를 출력하는 기능 추가

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,5 +1,9 @@
 package pairmatching.view;
 
-public class OutputView {
+public class OutputView extends IoPrinter {
 
+    public void printMatchingResultCrews(String matchedCrewNames) {
+        this.println("페어 매칭 결과입니다.");
+        this.println(matchedCrewNames);
+    }
 }
