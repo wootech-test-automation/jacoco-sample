a68cb991bfed2154d1882ef863f1e57b31abd87b
JaehongDev <println@kakao.com>
2022-12-09 15:28:02 +0900

    refactor (MatchingProcessStatus) : 매칭 처리하는 상태 추가

--- src/main/java/pairmatching/launcher/status/MatchingProcessStatus.java
+++ src/main/java/pairmatching/launcher/status/MatchingProcessStatus.java
@@ -1,12 +1,29 @@
 package pairmatching.launcher.status;
 
+import pairmatching.exception.BeforeMatchedCrewException;
+import pairmatching.exception.DuplicatedMatchingDivisionException;
 import pairmatching.launcher.context.PairmatchingContext;
+import pairmatching.view.OutputView;
 
 public class MatchingProcessStatus implements PairmatchingStatus {
     @Override
     public PairmatchingStatus next(PairmatchingContext pairmatchingContext) {
-        pairmatchingContext.matchPair();
-        return null;
+        var pairmatchingView = pairmatchingContext.getPairmatchingView();
+        var outputView = pairmatchingView.getOutputView();
+
+        pairmatchingContext.plusTryCount();
+        return this.getNextStatus(pairmatchingContext, outputView);
+    }
+
+    private PairmatchingStatus getNextStatus(PairmatchingContext pairmatchingContext, OutputView outputView) {
+        try {
+            outputView.printMatchingResultCrews(pairmatchingContext.matchPair());
+            return new SelectFeatureStatus();
+        } catch (DuplicatedMatchingDivisionException duplicatedMatchingDivisionException) {
+            return new AskRematchingStatus();
+        } catch (BeforeMatchedCrewException | IllegalStateException exception) {
+            return this;
+        }
     }
 
     @Override
