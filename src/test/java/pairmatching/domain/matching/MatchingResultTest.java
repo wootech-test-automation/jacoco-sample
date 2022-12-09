4737c37de09d2bd38dc5774067d6844db7bc204a
JaehongDev <println@kakao.com>
2022-12-09 10:10:21 +0900

    test (MatchingResultTest) : 매칭 결과에 대한 테스트코드 작성

---
+++ src/test/java/pairmatching/domain/matching/MatchingResultTest.java
@@ -0,0 +1,39 @@
+package pairmatching.domain.matching;
+
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static pairmatching.utils.TestUtils.convertStringToCrews;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import pairmatching.domain.code.Course;
+import pairmatching.exception.DuplicatedMatchingDivisionException;
+import pairmatching.exception.MatchingDivisionDidNotExists;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class MatchingResultTest {
+
+    @Test
+    void 중복되는_키가_있는_경우_에러를_발생시킵니다() {
+        var matchingResult = new MatchingResult();
+        var matchingDivision = new MatchingDivision("백엔드,레벨1,자동차경주");
+
+        assertThatThrownBy(() -> {
+            matchingResult.matchPair(matchingDivision, convertStringToCrews(Course.BACKEND, "가,나,다"));
+            matchingResult.matchPair(matchingDivision, convertStringToCrews(Course.BACKEND, "가,나,다"));
+        }).isInstanceOf(DuplicatedMatchingDivisionException.class);
+    }
+
+    @Test
+    void 키를_통해서_매칭_결과를_조회할때_데이터가_없는_경우_에러를_발생시킵니다() {
+        var matchingResult = new MatchingResult();
+        var matchingDivision = new MatchingDivision("백엔드,레벨1,자동차경주");
+
+        assertThatThrownBy(() -> {
+            matchingResult.findByMatchingDivision(matchingDivision);
+        }).isInstanceOf(MatchingDivisionDidNotExists.class);
+    }
+
+
+}
